ec95181b8a24f392f41795babf747d5a1d0f6ef7
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-01-01 23:14:16 +0700

    handling the was touched state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,45 +1,51 @@
-import { useRef, useState } from "react"
+import { useState, useEffect } from "react"
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState("")
-  const [enterNameIsValid, setEnterNameIsValid] = useState(true)
-
+  const [enterNameIsValid, setEnterNameIsValid] = useState(false)
+  //mặc định ban đầu thì trường này sẽ không được touch vào
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value)
   }
 
   const formSubmissionHandler = (event) => {
     event.preventDefault()
+    //Được hiểu là nếu người dùng ban đầu không thay đổi gì input và submit luôn thì tất cả các trường sẽ được hiểu là đã được touch
+    //trong trường hợp này thì ta chỉ có một trường thôi
+    setEnteredNameTouched(true)
     //Check truong hop la rong thi khong thuc hien lenh ben duoi
     if (enteredName.trim() === "") {
       setEnterNameIsValid(false)
       return
     }
     alert(enteredName)
-    console.log("nameInputRef", nameInputRef.current.value)
-    //trường hợp sau khi submit ta muốn clear giá trị trong trường input
-    //Có 2 cách đề làm:
-    // nameInputRef.current.value = "" //Cách này không khuyến khích vì nó đang chọc thẳng vào DOM
     setEnteredName("")
   }
+  const nameInputIsInvalid = !enterNameIsValid && enteredNameTouched
 
-  const nameInputClasses = enterNameIsValid
-    ? "form-control"
-    : "form-control invalid"
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control"
 
+  useEffect(() => {
+    //to do something
+    if (enterNameIsValid) {
+      //demo http request
+      console.log("send value to server")
+    }
+  }, [enterNameIsValid])
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           onChange={nameInputChangeHandler}
           type="text"
           id="name"
           value={enteredName}
         />
-        {!enterNameIsValid && (
+        {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty</p>
         )}
       </div>
