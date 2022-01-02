55fe8050c578ad555b5fb41a172561ab7ed8a50a
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-01-02 10:45:48 +0700

    BÀI 9. THỰC HÀNH FORM VALIDATION BASIC VỚI 2 TRƯỜNG LÀ NAME VÀ EMAIL

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,10 +3,22 @@ import { useState } from "react"
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("")
   const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+  const [enteredEmail, setEnteredEmail] = useState("")
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false)
+
   const enteredNameIsValid = enteredName.trim() !== ""
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
+  const validRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  const enteredEmailIsValid =
+    enteredEmail.trim() !== "" && enteredEmail.match(validRegex)
+  const emailInputIsInValid = !enteredEmailIsValid && enteredEmailTouched
+
   let formIsValid = false
-  if (enteredNameIsValid) {
+  if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true
   }
 
@@ -14,23 +26,39 @@ const SimpleInput = (props) => {
     setEnteredName(e.target.value)
   }
 
-  const nameInputBlurHandler = (event) => {
+  const nameInputBlurHandler = (e) => {
     setEnteredNameTouched(true)
+  }
+
+  const emailInputChangeHandler = (e) => {
+    setEnteredEmail(e.target.value)
+  }
+
+  const emailInputBlurHandler = (e) => {
+    setEnteredEmailTouched(true)
   }
 
   const formSubmissionHandler = (event) => {
     event.preventDefault()
-    //Được hiểu là nếu người dùng ban đầu không thay đổi gì input và submit luôn thì tất cả các trường sẽ được hiểu là đã được touch
-    //trong trường hợp này thì ta chỉ có một trường thôi
-    setEnteredNameTouched(true)
-    //Check truong hop la rong thi khong thuc hien lenh ben duoi
-    if (!enteredNameIsValid) return
-    alert(enteredName)
+
+    const values = {
+      name: enteredName,
+      email: enteredEmail,
+    }
+    alert(JSON.stringify(values))
+
     setEnteredName("")
     setEnteredNameTouched(false)
+
+    setEnteredEmail("")
+    setEnteredEmailTouched(false)
   }
 
   const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control"
+
+  const emailInputClasses = emailInputIsInValid
     ? "form-control invalid"
     : "form-control"
 
@@ -47,6 +75,19 @@ const SimpleInput = (props) => {
         />
         {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your E-mail</label>
+        <input
+          onBlur={emailInputBlurHandler}
+          onChange={emailInputChangeHandler}
+          type="email"
+          id="email"
+          value={enteredEmail}
+        />
+        {emailInputIsInValid && (
+          <p className="error-text">Email must be valid</p>
         )}
       </div>
       <div className="form-actions">
