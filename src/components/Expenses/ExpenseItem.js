4d3d16b0c5350cf6288cac6da27a09a2bde11c8d
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-01-05 05:09:12 +0700

    how to useState hooks with event onClick lesson 50

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,20 @@
+import { useState } from "react"
 import Card from "../UI/Card"
 import ExpenseDate from "./ExpenseDate"
 import "./ExpenseItem.css"
 function ExpenseItem(props) {
   const { title, amount, date } = props.expense
-
+  const [heading, setHeading] = useState(title)
+  const clickHandler = (e) => {
+    setHeading("Updated")
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{heading}</h2>
         <div className="expense-item__price">${amount}</div>
+        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   )
