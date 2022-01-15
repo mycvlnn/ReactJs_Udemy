6e593c10bc13d0ec74bd47b46a246220b296c03b
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-01-15 21:52:15 +0700

    useReducer with useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,30 +1,72 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useReducer } from "react"
 
 import Card from "../UI/Card/Card"
 import classes from "./Login.module.css"
 import Button from "../UI/Button/Button"
-
+const initialState = {
+  enteredEmail: "",
+  enteredPassword: "",
+  emailIsValid: null,
+  passwordIsValid: null,
+}
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "EMAIL_INPUT": {
+      return {
+        ...state,
+        enteredEmail: payload,
+        emailIsValid: state.enteredEmail.includes("@"),
+      }
+    }
+    case "EMAIL_BLUR": {
+      return { ...state, emailIsValid: state.enteredEmail.includes("@") }
+    }
+    case "PASSWORD_INPUT": {
+      return {
+        ...state,
+        enteredPassword: payload,
+        passwordIsValid: state.enteredPassword.trim().length > 6,
+      }
+    }
+    case "PASSWORD_BLUR": {
+      return {
+        ...state,
+        passwordIsValid: state.enteredPassword.trim().length > 6,
+      }
+    }
+    default:
+      return state
+  }
+}
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("")
-  const [emailIsValid, setEmailIsValid] = useState()
-  const [enteredPassword, setEnteredPassword] = useState("")
-  const [passwordIsValid, setPasswordIsValid] = useState()
   const [formIsValid, setFormIsValid] = useState(false)
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { enteredEmail, enteredPassword, emailIsValid, passwordIsValid } = state
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value)
+    dispatch({
+      type: "EMAIL_INPUT",
+      payload: event.target.value,
+    })
   }
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value)
+    dispatch({
+      type: "PASSWORD_INPUT",
+      payload: event.target.value,
+    })
   }
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"))
+    dispatch({
+      type: "EMAIL_BLUR",
+    })
   }
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6)
+    dispatch({
+      type: "PASSWORD_BLUR",
+    })
   }
 
   const submitHandler = (event) => {
@@ -33,10 +75,16 @@ const Login = (props) => {
   }
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    )
-  }, [enteredEmail, enteredPassword])
+    const idTimer = setTimeout(() => {
+      //Giả sử trong đây là http request thì chúng ta đã cải thiện nó thay vì gửi hàng tá request lên server
+      //Thì chúng ta chỉ cần gửi một lần duy nhất sau khi người dùng không bấm phím nữa
+      setFormIsValid(emailIsValid && passwordIsValid)
+    }, 400)
+    //cleanup function ham nay se duoc chay truoc khi callback cua useEffect dươc chạy ngoại trừ lần đầu tiên
+    return () => {
+      clearTimeout(idTimer)
+    }
+  }, [emailIsValid, passwordIsValid])
 
   return (
     <Card className={classes.login}>
