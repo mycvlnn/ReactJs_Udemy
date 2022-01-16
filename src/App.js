a9485352f2251d6ebfe7eb212fcb67ad5df69c70
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-01-16 08:19:13 +0700

    useContext Hook

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         isLoggedIn: isLoggedIn,
       }}
     >
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
