b444722db9db55de79e7dc14d6bcca86986180b4
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-01-18 23:15:11 +0700

    init react-base

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import Login from "./components/Login/Login"
-import Home from "./components/Home/Home"
-import MainHeader from "./components/MainHeader/MainHeader"
-import React, { useContext } from "react"
-import AuthContext from "./store/authContext"
+import React from "react"
 function App() {
-  const ctx = useContext(AuthContext)
-  return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!ctx.isLoggedIn && <Login />}
-        {ctx.isLoggedIn && <Home />}
-      </main>
-    </React.Fragment>
-  )
+  return <React.Fragment></React.Fragment>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from "react-dom"
 
 import "./index.css"
 import App from "./App"
-import { AuthContextProvider } from "./store/authContext"
 
-ReactDOM.render(
-  <AuthContextProvider>
-    <App />
-  </AuthContextProvider>,
-  document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"))
