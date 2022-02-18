e48009d25ed01b9a184b4844bf89a92c02399dba
le ngoai ngu <lengoaingu@gmail.com>
2022-02-18 11:27:25 +0700

    Connect Redux t State

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,34 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initState = {
   counter: 0,
   showToggle: true,
 };
 
-const reducer = (state = initState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showToggle: state.showToggle,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showToggle: state.showToggle,
-    };
-  }
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initState,
+  reducers: {
+    //adding method
+    increment(state) {
+      //Điều này đã bị cấm trước đó khi chúng ta không dùng toolkit.
+      //Nhưng đối với toolkit bên trong nó sử dụng một gói khác có tên là imgur. Nó sẽ phát hiện code như thế này và sẽ tự động  sao chép trạng thái hiện tại. Tạo một đối tượng state mới giữ tất cả trạng thái cũ mà chúng ta không chỉnh sửa và ghi đè lên trạng thái mà chúng ta đang chỉnh sửa một cách bất biến
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showToggle = !state.showToggle;
+    },
+  },
+});
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showToggle: state.showToggle,
-    };
-  }
-  if (action.type === "toggle") {
-    return {
-      showToggle: !state.showToggle,
-      counter: state.counter,
-    };
-  }
-  return state;
-};
-
-const store = createStore(reducer);
+const store = configureStore({
+  reducer: counterSlice.reducer, //Ngoài ra chúng ta cũng có thể truyền vào là object trong trường hợp chúng ta có nhiều hơn 1 reducer.
+});
 
 export default store;
