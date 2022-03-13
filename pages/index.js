db98c1f5ecc9d57b7d3bd2b0c5bf50194e8fe7f4
le ngoai ngu <lengoaingu@gmail.com>
2022-03-13 16:59:29 +0700

    330. outputing list meetups

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "The first meetup",
+    image: "https://picsum.photos/1000",
+    address: "Thanh Loi Pho, Bo Hong Son",
+  },
+  {
+    id: "m2",
+    title: "The second meetup",
+    image: "https://picsum.photos/1000",
+    address: "Thanh Loi Pho, Bo Hong Son",
+  },
+];
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_DATA} />;
+};
+
+export default HomePage;
