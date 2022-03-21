34ea397951a15a77ebf2228753b38d5041cdee95
le ngoai ngu <lengoaingu@gmail.com>
2022-03-21 22:02:24 +0700

    352. Preparing Demo Project

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+class App extends Component {
+  state = {
+    modalIsOpen: false,
+  };
+
+  showModal = () => {
+    this.setState({
+      modalIsOpen: true,
+    });
+  };
+
+  hideModal = () => {
+    this.setState({
+      modalIsOpen: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <Modal closed={this.hideModal} show={this.state.modalIsOpen} />
+        <Backdrop show={this.state.modalIsOpen} />
+        <button onClick={this.showModal} className="Button">
+          Open Modal
+        </button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
