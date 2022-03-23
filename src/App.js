f7ab40add80eed789060409a1e8ba83bf877add8
le ngoai ngu <lengoaingu@gmail.com>
2022-03-23 21:41:16 +0700

    356. Using React transtion group

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
+const defaultStyle = {
+  transition: "opacity 300ms ease-in-out",
+  opacity: 0,
+};
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+};
+
 class App extends Component {
   state = {
     modalIsOpen: false,
+    toggle: false,
   };
 
   showModal = () => {
@@ -25,7 +39,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <button
+          onClick={() =>
+            this.setState((prevState) => ({ toggle: !prevState.toggle }))
+          }
+        >
+          Toggle
+        </button>
+        <Transition
+          mountOnEnter
+          unmountOnExit
+          in={this.state.toggle}
+          timeout={1000}
+        >
+          {(state) => (
+            <div
+              style={{
+                background: "orange",
+                width: 200,
+                height: 200,
+                margin: "auto",
+                ...defaultStyle,
+                ...transitionStyles[state],
+              }}
+            >
+              BOX
+            </div>
+          )}
+        </Transition>
         <h1>React Animations</h1>
+
         {this.state.modalIsOpen && (
           <Modal closed={this.hideModal} show={this.state.modalIsOpen} />
         )}
